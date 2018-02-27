ad7372afe3b6cb70af3602ce7acd941a6ac4938b
nikolehto <niko.lehto@mbnet.fi>
2018-02-27 11:09:25 +0200

    tweet search to async task

--- app/src/main/java/dot/tweetsearchexercise/MainActivity.java
+++ app/src/main/java/dot/tweetsearchexercise/MainActivity.java
@@ -2,6 +2,7 @@ package dot.tweetsearchexercise;
 
 import android.content.Intent;
 import android.nfc.Tag;
+import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -22,67 +23,27 @@ public class MainActivity extends AppCompatActivity {
     List<Status> statuses = null;
 
     ListView listView;
+    MyTwitterTask myTwitterTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+        //StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        //StrictMode.setThreadPolicy(policy);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.listview);
 
-        if(listView != null)
-        {}
-        else
-        {
-            Log.d("VOICE says", "listview is null");
-        }
-
         initializeCb();
-        twitter = new TwitterFactory(cb.build()).getInstance();
+        //
 
-        getTweets("google");
+        myTwitterTask = new MyTwitterTask();
+        myTwitterTask.execute("google");
+        //getTweets("google");
     }
 
-    protected void getTweets(String user)
-    {
-        Paging paging = new Paging(1, 20);
 
-        try {
-            statuses = twitter.getUserTimeline(user, paging);
-        } catch (TwitterException e) {
-            e.printStackTrace();
-        }
-
-        Log.d("TWITTER SAYS: ", "Total: "+statuses.size());
-        printTweets();
-    }
-
-    protected void printTweets()
-    {
-        //String[] stringArray;
-
-        String[] listItems = new String[statuses.size()];
-
-        for(int i = 0; i < statuses.size(); i++)
-        {
-            Status status = statuses.get(i);
-            listItems[i] = status.getText();
-            // status.toString();
-        }
-
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
-
-        listView.setAdapter(adapter);
-
-        //&&listView.setAdapter();
-
-        /*
-
-        */
-    }
 
     protected void initializeCb()
     {
@@ -92,6 +53,69 @@ public class MainActivity extends AppCompatActivity {
         cb.setOAuthConsumerSecret(getString(R.string.consumersecret));
         cb.setOAuthAccessToken(getString(R.string.accesstoken));
         cb.setOAuthAccessTokenSecret(getString(R.string.accessecret));
+    }
+
+
+    private class MyTwitterTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            try
+            {
+                if(twitter != null)
+                {
+                }
+                else
+                {
+                    twitter = new TwitterFactory(cb.build()).getInstance();
+                }
+
+                String user = params[0];
+
+                getTweets(user);
+                return "OK";
+            }
+            catch (TwitterException e) {
+                e.printStackTrace();
+                return "Error";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            printTweets();
+        }
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
+
+
+        protected void getTweets(String user) throws TwitterException {
+            Paging paging = new Paging(1, 20);
+
+            statuses = twitter.getUserTimeline(user, paging);
+
+            Log.d("TWITTER SAYS: ", "Total: "+statuses.size());
+        }
+
+        protected void printTweets()
+        {
+            String[] listItems = new String[statuses.size()];
+
+            for(int i = 0; i < statuses.size(); i++)
+            {
+                twitter4j.Status status = statuses.get(i);
+                listItems[i] = status.getText();
+            }
+
+            ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, listItems);
+
+            listView.setAdapter(adapter);
+        }
     }
 
 }
